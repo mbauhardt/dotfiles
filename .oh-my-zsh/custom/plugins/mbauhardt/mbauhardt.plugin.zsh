283f6c267771e792d0c52744e8cbdc0a941f4b06
Marko Bauhardt <marko.bauhardt@mailbox.org>
2017-01-31 22:55:46 +0100

    update mbauhardt plugin

--- .oh-my-zsh/custom/plugins/mbauhardt/mbauhardt.plugin.zsh
+++ .oh-my-zsh/custom/plugins/mbauhardt/mbauhardt.plugin.zsh
@@ -1,2 +1,24 @@
 ZSH_THEME="mbauhardt"
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+alias java_ls='/usr/libexec/java_home -V 2>&1 | grep -E "\d.\d.\d[,_]" | cut -d , -f 1 | colrm 1 4 | grep -v Home'
+function setjdk() {
+  if [ $# -ne 0 ]; then
+    removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+    if [ -n "${JAVA_HOME+x}" ]; then
+      removeFromPath $JAVA_HOME
+    fi
+    export JAVA_HOME=`/usr/libexec/java_home -v $@`
+    export PATH=$JAVA_HOME/bin:$PATH
+  fi
+  echo JAVA_HOME set to $JAVA_HOME
+  java -version
+ }
+
+function removeFromPath() {
+  export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
+}
+
+setjdk 1.8
+export PATH=$HOME/bin:$PATH
+
