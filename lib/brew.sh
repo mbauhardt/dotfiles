683dd543ee1dfdd63da1806d55ff51bf2229f952
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-01-02 10:35:35 +0100

    Added brew installation

--- lib/brew.sh
+++ lib/brew.sh
@@ -1,12 +1,3 @@
-
-_installBrew() {
-  echo "\033[0;33mCheck if brew is installed.\033[0m"
-  brew info 2> /dev/null
-  [[ $? -eq 0 ]] && echo "\033[0;33mBrew is already installed. Skip the installation.\033[0m" && return 1
-  echo "Brew is not yet installed. Performing the installation now."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-}
-
 _installFormula() {
   [[ "$#" != 1 ]] && return 1
   local formula=$1
