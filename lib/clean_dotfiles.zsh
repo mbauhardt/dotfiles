960f621741cf83ecda2f4678f742cf2e0615948e
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-01-31 16:47:26 +0100

    use env variable to know where the dotfiles are located

--- lib/clean_dotfiles.zsh
+++ lib/clean_dotfiles.zsh
@@ -1,19 +1,18 @@
 zstyle ":dotfiles:load" dmodules 'bookmarks' 'chat' 'dev' 'gtd' 'mail-work' 'neovim' 'privacy' 'shell' 'system'
 
 zstyle -a ":dotfiles:load" dmodules dots
-printf "Link all files from the given modules '%s'\n" "${dots}"
+printf "Delete all symblic links from the given modules '%s'\n" "${dots}"
 
 for dot in "$dots[@]";do 
-  
+
   [[ ! -d ${DOTFILESDIR:-$HOME/dotfiles}/modules/$dot/files ]] && continue
   pushd ${DOTFILESDIR:-$HOME/dotfiles}/modules/$dot/files > /dev/null
   for f in **/*(D.);do 
     local lnk=$HOME/$f
     local gitFile=${f:a}
-    [[ -a $lnk ]] && printf "[%s] File '%s' already exist.\n" "${dot}" "${lnk}" && continue
-    printf "[%s] Create symbolic link '%s' => '%s'.\n" "${dot}" "${lnk}" "${gitFile}"
-    mkdir -p ${lnk:h}
-    ln -s ${gitFile} $lnk 
+    [[ ! -h $lnk ]] && printf "[%s] File '%s' is not a symbolic link.\n" "${dot}" "${lnk}" && continue
+    printf "[%s] Delete symbolic link '%s'.\n" "${dot}" "${lnk}" 
+    rm $lnk 
   ;done
   popd > /dev/null
 
