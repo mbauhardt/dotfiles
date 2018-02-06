b9bfd9a5dd7eea1eb9bf0d03dbfb0c0959ee933a
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-02-06 17:52:04 +0100

    delete deprecated files

--- lib/brew.sh
+++
@@ -1,9 +0,0 @@
-_installFormula() {
-  [[ "$#" != 1 ]] && return 1
-  local formula=$@
-  brew list $formula 1> /dev/null 2> /dev/null
-  [[ $? -eq 0 ]] && echo "\033[0;33m$formula is already installed. Skip the installation.\033[0m" && return 1
-  echo "$formula is not yet installed. Performing the installation now."
-  brew install $formula 1>> logs/brew_install.log 2>> logs/brew_install_error.log
-}
-
