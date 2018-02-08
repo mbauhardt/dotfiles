34068f7ad4081c529f7a0f72367dcef86ab9d22b
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-02-08 20:38:56 +0100

    improved delete dotfiles

--- lib/clean_dotfiles.zsh
+++ lib/clean_dotfiles.zsh
@@ -1,3 +1,10 @@
+_deleteDotfiles() {
+  local lnk=$1
+  #[[ ! -h $lnk ]] &&  return 2;
+  rm $lnk
+  return $?
+}
+
 zstyle -a ":dotfiles:load" dmodules dots
 printf "Delete all symblic links from the given modules '%s'\n" "${dots}"
 
@@ -7,10 +14,10 @@ for dot in "$dots[@]";do
   pushd ${DOTFILESDIR:-$HOME/dotfiles}/modules/$dot/files > /dev/null
   for f in **/*(D.);do 
     local lnk=$HOME/$f
-    local gitFile=${f:a}
-    [[ ! -h $lnk ]] && printf "[%s] File '%s' is not a symbolic link.\n" "${dot}" "${lnk}" && continue
-    printf "[%s] Delete symbolic link '%s'.\n" "${dot}" "${lnk}" 
-    rm $lnk 
+    printf "[%s] Delete symbolic link '%s'." "${dot}" "${lnk}"
+    _deleteDotfiles $lnk
+    local ret=$?
+    ([[ $ret -eq 0 ]] && printf "[\033[0;32mOK\033[0m]\n") || ([[ $ret -eq 1 ]] && printf "[\033[0;31mERROR\033[0m]\n") || ([[ $ret -eq 2 ]] && printf "[\033[0;33mSKIP\033[0m]\n")
   ;done
   popd > /dev/null
 
