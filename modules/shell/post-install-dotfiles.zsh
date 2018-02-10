4add3462e9c2d68b00ff4b412447bdeeb0444530
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-02-10 21:32:25 +0100

    Switched from ohmyzsh to prezto

---
+++ modules/shell/post-install-dotfiles.zsh
@@ -0,0 +1,4 @@
+setopt EXTENDED_GLOB
+for rcfile in ${DOTFILESDIR:-$HOME/dotfiles}/modules/shell/externals/prezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
