2bcdbdb70c211397e8d3b83d5c6ac965c2d81624
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-02-10 15:26:49 +0100

    clone git repos via git protocol instead https

--- modules/shell/install-software.zsh
+++ modules/shell/install-software.zsh
@@ -1,2 +1,5 @@
 zstyle ":dotfiles:modules:shell:software" brew 'csshx' 'fzf' 'midnight-commander' 'ranger' 'p7zip' 's3cmd' 'tmux' 'tree' 'the_silver_searcher'
-zstyle ":dotfiles:modules:shell:software" git 'https://github.com/jimeh/tmux-themepack.git' "$HOME/.tmux-themepack" "https://github.com/chriskempson/base16-shell.git" "$HOME/.config/base16-shell"
+
+zstyle ":dotfiles:modules:shell:software" git \
+	"git@github.com:jimeh/tmux-themepack.git" "$HOME/.tmux-themepack" \
+	"git@github.com:chriskempson/base16-shell.git" "$HOME/.config/base16-shell"
