4ee797b8367240824d6594bcbd0c18020233571b
Marko Bauhardt <marko.bauhardt@mailbox.org>
2018-12-09 10:32:58 +0100

    update private mail setup to new folder structure

--- modules/mail/post-install-dotfiles.zsh
+++ modules/mail/post-install-dotfiles.zsh
@@ -1,4 +1,4 @@
 print  "Decrypt notmuch post hook to tag emails.\n"
-keybase decrypt -i ~/Mail/.notmuch/hooks/post-new.keybase -o ~/Mail/.notmuch/hooks/post-new
-chmod u+x ~/Mail/.notmuch/hooks/post-new
+keybase decrypt -i ~/z/documents/mail/.notmuch/hooks/post-new.keybase -o ~/Mail/.notmuch/hooks/post-new
+chmod u+x ~/z/documents/mail/.notmuch/hooks/post-new
 
