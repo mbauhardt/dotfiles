030c0d5b9ad2625596ce40b025b092d29a70c537
Marko Bauhardt <marko.bauhardt@mailbox.org>
2019-02-10 21:05:47 +0100

    removed more prezto related lines

--- modules/shell/post-install-dotfiles.zsh
+++ modules/shell/post-install-dotfiles.zsh
@@ -1,7 +1,2 @@
-setopt EXTENDED_GLOB
-for rcfile in ${DOTFILESDIR:-$HOME/dotfiles}/modules/shell/externals/prezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
 $(brew --prefix)/opt/fzf/install
 
