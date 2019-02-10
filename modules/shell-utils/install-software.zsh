a113fffbd5a5f0a434391c52441c608979d0e19e
Marko Bauhardt <marko.bauhardt@mailbox.org>
2019-02-10 21:14:00 +0100

    renamed shell module to shell-utils

--- modules/shell-utils/install-software.zsh
+++ modules/shell-utils/install-software.zsh
@@ -1,5 +1,5 @@
-zstyle ":dotfiles:modules:shell:software" brew 'csshx' 'fzf' 'midnight-commander' 'ranger' 'p7zip' 's3cmd' 'tmux' 'tree' 'the_silver_searcher' 'htop' 'glances' 'pdfgrep' 'fasd'
-zstyle ":dotfiles:modules:shell:software" cask 'iterm2'
+zstyle ":dotfiles:modules:shell-utils:software" brew 'csshx' 'fzf' 'midnight-commander' 'ranger' 'p7zip' 's3cmd' 'tmux' 'tree' 'the_silver_searcher' 'htop' 'glances' 'pdfgrep' 'fasd'
+zstyle ":dotfiles:modules:shell-utils:software" cask 'iterm2'
 
 #zstyle ":dotfiles:modules:shell:software" git \
 #	"git@github.com:jimeh/tmux-themepack.git" "$HOME/.tmux-themepack" \
