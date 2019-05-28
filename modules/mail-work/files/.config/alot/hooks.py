571458b354e4fd3db7d9ab8697b47ca27e4f9b86
Marko Bauhardt <marko.bauhardt@mailbox.org>
2019-05-28 17:12:25 +0200

    added keybase encryption, tmux plugin, hyper

--- modules/mail-work/files/.config/alot/hooks.py
+++ modules/mail-work/files/.config/alot/hooks.py
@@ -13,3 +13,17 @@ def pre_global_refresh(**kwargs):
   logging.info('Syncing mail with offlineimap')
   ui.notify("fetchinig email..")  
   #subprocess.call("offlineimap -o".split(),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+def enc(userid):
+  e = ui.current_buffer.envelope
+  msg = e.body
+  try:
+      e.body = subprocess.check_output(['keybase', 'encrypt', '-m', '"'+
+msg +'"', userid], stderr=subprocess.STDOUT)
+  except Exception as e:
+      ui.notify('Failure while encryption:' + str(e), priority='error')
+  else:
+      ui.notify('Encryption done...')
+
+  ui.current_buffer.rebuild()
+  
--- modules/shell-utils/install-software.zsh
+++ modules/shell-utils/install-software.zsh
@@ -1,5 +1,5 @@
 zstyle ":dotfiles:modules:shell-utils:software" brew 'git' 'csshx' 'fzf' 'midnight-commander' 'ranger' 'p7zip' 's3cmd' 'tmux' 'tree' 'the_silver_searcher' 'htop' 'glances' 'pdfgrep' 'fasd' 'nnn' 'atool'
-zstyle ":dotfiles:modules:shell-utils:software" cask 'iterm2'
+zstyle ":dotfiles:modules:shell-utils:software" cask 'hyper'
 
 #zstyle ":dotfiles:modules:shell:software" git \
 #	"git@github.com:jimeh/tmux-themepack.git" "$HOME/.tmux-themepack" \
