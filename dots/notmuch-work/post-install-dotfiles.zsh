c4b5eac02450381b5f2e0149e37ff4e955c49be0
Marko Bauhardt <marko.bauhardt@mailbox.org>
2020-01-25 16:52:02 +0100

    fix gmail sync trash behaviour

--- dots/notmuch-work/post-install-dotfiles.zsh
+++ dots/notmuch-work/post-install-dotfiles.zsh
@@ -1,3 +1,6 @@
 print "Initialize Notmuch Address Cache"
 $DOTFILESDIR/submodules/notmuch-address-cache/bin/notmuch-address-cache rebuild --text
 
+print "Decrypt notmutch post new hook"
+keybase decrypt -i ~/z/documents/mail/.notmuch/hooks/post-new.keybase -o ~/z/documents/mail/.notmuch/hooks/post-new
+
