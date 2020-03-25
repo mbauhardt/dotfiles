651f1b2ce19a36782884d3c66f5301ef6afbaa60
Marko Bauhardt <marko.bauhardt@mailbox.org>
2020-03-25 18:01:24 +0100

    removed osx installer

--- lib/install_software.zsh
+++ lib/install_software.zsh
@@ -1,6 +1,25 @@
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  source ${DOTFILESDIR:-$HOME/dotfiles}/lib/install_software_linux.zsh
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  source ${DOTFILESDIR:-$HOME/dotfiles}/lib/install_software_osx.zsh
+local install_command
+
+icmd=(sudo)
+if grep ID /etc/os-release | grep -qE "fedora"; then
+  icmd+=(dnf)
+  icmd+=(install)
+elif grep ID /etc/os-release | grep -q void; then
+  icmd+=(xbps-install)
+else 
+  echo "Sorry, we don't recognize your OS."
+  exit 1
 fi
+
+zstyle -a ":dotfiles:install" app apps
+
+printf "Install aps from the given modules '%s'\n" "${apps}"
+read "con?Do you want to continue? (y/n)"
+[[ "$con" =~ ^[Yy]$ ]] || exit 1
+
+for a in "$apps[@]";do
+  printf "Installing '%s'...\n" "${a}"
+  "${icmd[@]}" ${a}
+done
+
 
