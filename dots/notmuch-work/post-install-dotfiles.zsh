071c6b82790409320d94b3f40b601b5314769d24
Marko Bauhardt <marko.bauhardt@mailbox.org>
2021-04-26 16:29:33 +0200

    rework mail setup and folder

--- dots/notmuch-work/post-install-dotfiles.zsh
+++ dots/notmuch-work/post-install-dotfiles.zsh
@@ -1,6 +1,6 @@
 print "Initialize Notmuch Address Cache"
-$DOTFILESDIR/submodules/notmuch-address-cache/bin/notmuch-address-cache rebuild --text
+#$DOTFILESDIR/submodules/notmuch-address-cache/bin/notmuch-address-cache rebuild --text
 
 print "Decrypt notmutch post new hook"
-keybase decrypt -i ~/z/documents/mail/.notmuch/hooks/post-new.keybase -o ~/z/documents/mail/.notmuch/hooks/post-new
+#keybase decrypt -i ~/var/mail/datameer.com/.notmuch/hooks/post-new.keybase -o ~/var/mail/datameeer.com/.notmuch/hooks/post-new
 
