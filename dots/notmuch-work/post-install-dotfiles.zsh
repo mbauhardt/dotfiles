13e422722f13d2344f67a7c9393b2c2dc01adad7
Marko Bauhardt <mb@datameer.com>
2022-02-09 14:56:42 +0100

    notmuch batch tagging

--- dots/notmuch-work/post-install-dotfiles.zsh
+++ dots/notmuch-work/post-install-dotfiles.zsh
@@ -2,5 +2,5 @@ print "Initialize Notmuch Address Cache"
 #$DOTFILESDIR/submodules/notmuch-address-cache/bin/notmuch-address-cache rebuild --text
 
 print "Decrypt notmutch post new hook"
-#keybase decrypt -i ~/var/mail/datameer.com/.notmuch/hooks/post-new.keybase -o ~/var/mail/datameeer.com/.notmuch/hooks/post-new
+#keybase decrypt -i ~/var/mail/datameer.com/.notmuch/hooks/mail-tags.keybase -o ~/var/mail/datameeer.com/.notmuch/hooks/mail-tags
 
